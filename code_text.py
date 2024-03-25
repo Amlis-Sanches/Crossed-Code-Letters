bed7ed964a021ee5302def1ea4dfb4e3cf819170
Nathaniel Horn <132115518+Amlis-Sanches@users.noreply.github.com>
2024-03-24 21:07:22 -0500

    Moved functions to seperate file for testing and future use.

--- code_text.py
+++ code_text.py
@@ -4,6 +4,7 @@ import docx
 import math
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont, ImageChops
+from text_clean import text_clean, extract_text
 
 '''
 Main function to handle the workflow.
@@ -39,88 +40,6 @@ def main():
         combine_letter(blue_list[i], red_list[i], i)
     
     print("Crossed Letter Generated!")
-
-
-# Function to extract text from various formats
-def extract_text(file_path, file_type):
-    # Identify file type. If text, return text
-    try:
-        match file_type:
-            case "text":
-                with open(file_path, "r") as file:
-                    text = file.read()
-            case "word":
-                doc = docx.Document(file_path)
-                full_text = []
-                for para in doc.paragraphs:
-                    full_text.append(para.text)
-                text = "\n".join(full_text)
-                '''
-            case "pdf":
-                pdf_file = open(file_path, "rb")
-                pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-                full_text = []
-                for page in range(pdf_reader.numPages):
-                    page_obj = pdf_reader.getPage(page)
-                    full_text.append(page_obj.extractText())
-                text = "\n".join(full_text)
-                '''
-            case _:
-                text = "Program Error"
-    except:
-        text = "Error! Document not identified. Please try again."
-    #return just a string with all the text from the file
-    return text
-    
-def text_clean(text):
-    # Clean text for generation
-    cleaned_text = text.replace('\n', '')
-    cleaned_text = cleaned_text.replace('\t', '')
-    
-    
-    # Shape test to fit for the desired image
-    for i in range(0, len(cleaned_text), 80):
-        if i + 1 < len(cleaned_text):  # Ensure i+1 is a valid index
-            if cleaned_text[i] in ' .,?!:;':
-                cleaned_text = cleaned_text[:i] + '\n' + cleaned_text[i:]
-            elif (cleaned_text[i].isalpha() or cleaned_text[i] == "'") and cleaned_text[i+1] in " .,?!:;'":
-                cleaned_text = cleaned_text[:i+1] + '\n' + cleaned_text[i+1:]
-            elif (cleaned_text[i].isalpha() or cleaned_text[i] == "'") and (cleaned_text[i].isalpha() or cleaned_text[i] == "'"):
-                for j in range(i, max(0, i-80), -1):  # Iterate backwards from i
-                    if cleaned_text[j] == ' ':
-                        cleaned_text = cleaned_text[:j] + '\n' + cleaned_text[j+1:]
-                        i = i-j
-                        break  # Exit the loop once a space is found
-
-    # Check if the last line is longer than 80 characters
-    last_newline = cleaned_text.rfind('\n')
-    if len(cleaned_text) - last_newline > 80:
-        for j in range(len(cleaned_text), last_newline, -1):
-            if cleaned_text[j] == ' ':
-                cleaned_text = cleaned_text[:j] + '\n' + cleaned_text[j+1:]
-                break  # Exit the loop once a space is found
-
-
-    # Count the total number of lines and determine how many images will be processed
-    total_lines = len(cleaned_text.split('\n'))
-    # split lines after you reach 32 lines so the \n fits the image
-    half_length = 30 #Set to a specific number to fit the desired image
-    total_image = round(total_lines // (half_length * 2))
-
-    #create a red and blue list to hold the text
-    total_image = math.ceil(total_lines / (half_length * 2))
-    blue_list = [''] * total_image
-    red_list = [''] * total_image
-
-    cleaned_text_lines = cleaned_text.split('\n')
-    counter = 0
-    for i in range(0, len(cleaned_text_lines), (half_length*2)):
-        if i % half_length == 0:
-            blue_list[counter] = '\n'.join(cleaned_text_lines[:half_length])
-            red_list[counter] = '\n'.join(cleaned_text_lines[half_length:])
-            counter += 1
-        cleaned_text_lines = cleaned_text_lines[(half_length*2):]
-    return blue_list, red_list, total_image
 
 def combine_letter(text1, text2, i, j):
     '''
---
+++ text_clean.py
@@ -0,0 +1,83 @@
+import math
+import docx
+import re
+
+def main():
+    file_path = 'C:\Users\natha\Documents\GitHub\Crossed-Code-Letters\File Examples\Story_example.txt'
+    text = extract_text(file_path)
+    blue_list, red_list, num_of_images = text_clean(text)
+
+def extract_text(file_path):
+    # Identify file type. If text, return text
+    try:
+        match = re.search(r'\.(\w+)$', file_path)
+        file_type = match.group(1) if match else None
+        
+        match file_type:
+            case "txt":
+                with open(file_path, "r") as file:
+                    text = file.read()
+                    
+            case "docx":
+                doc = docx.Document(file_path)
+                full_text = []
+                for para in doc.paragraphs:
+                    full_text.append(para.text)
+                text = "\n".join(full_text)
+                
+            case _:
+                text = "Program Error"
+    except:
+        text = "Error! Document not identified. Please try again."
+    #return just a string with all the text from the file
+    return text
+
+def text_clean(text):
+    # Clean text for generation
+    cleaned_text = text.replace('\n', '')
+    cleaned_text = cleaned_text.replace('\t', '')
+    
+    
+    # Shape test to fit for the desired image
+    for i in range(0, len(cleaned_text), 80):
+        if i + 1 < len(cleaned_text):  # Ensure i+1 is a valid index
+            if cleaned_text[i] in ' .,?!:;':
+                cleaned_text = cleaned_text[:i] + '\n' + cleaned_text[i:]
+            elif (cleaned_text[i].isalpha() or cleaned_text[i] == "'") and cleaned_text[i+1] in " .,?!:;'":
+                cleaned_text = cleaned_text[:i+1] + '\n' + cleaned_text[i+1:]
+            elif (cleaned_text[i].isalpha() or cleaned_text[i] == "'") and (cleaned_text[i].isalpha() or cleaned_text[i] == "'"):
+                for j in range(i, max(0, i-80), -1):  # Iterate backwards from i
+                    if cleaned_text[j] == ' ':
+                        cleaned_text = cleaned_text[:j] + '\n' + cleaned_text[j+1:]
+                        i = i-j
+                        break  # Exit the loop once a space is found
+
+    # Check if the last line is longer than 80 characters
+    last_newline = cleaned_text.rfind('\n')
+    if len(cleaned_text) - last_newline > 80:
+        for j in range(len(cleaned_text), last_newline, -1):
+            if cleaned_text[j] == ' ':
+                cleaned_text = cleaned_text[:j] + '\n' + cleaned_text[j+1:]
+                break  # Exit the loop once a space is found
+
+
+    # Count the total number of lines and determine how many images will be processed
+    total_lines = len(cleaned_text.split('\n'))
+    # split lines after you reach 32 lines so the \n fits the image
+    half_length = 30 #Set to a specific number to fit the desired image
+    total_image = round(total_lines // (half_length * 2))
+
+    #create a red and blue list to hold the text
+    total_image = math.ceil(total_lines / (half_length * 2))
+    blue_list = [''] * total_image
+    red_list = [''] * total_image
+
+    cleaned_text_lines = cleaned_text.split('\n')
+    counter = 0
+    for i in range(0, len(cleaned_text_lines), (half_length*2)):
+        if i % half_length == 0:
+            blue_list[counter] = '\n'.join(cleaned_text_lines[:half_length])
+            red_list[counter] = '\n'.join(cleaned_text_lines[half_length:])
+            counter += 1
+        cleaned_text_lines = cleaned_text_lines[(half_length*2):]
+    return blue_list, red_list, total_image
