f0d2db309b2be11096f08935f61b71402aa33f0b
Nathaniel Horn <132115518+Amlis-Sanches@users.noreply.github.com>
2024-04-01 16:07:22 -0500

    added a example of a checking code.

--- symbol_checker.py
+++ symbol_checker.py
@@ -17,9 +17,19 @@ def list_directory(directory):
         for name in file_names:
             writer.writerow([name])
 
-def check_directory(directory):
+def check_directory(directory, file_list):
     # Get a list of all file names in the directory
-    file_names = os.listdir(directory)
+    directory_files = os.listdir(directory)
+
+    # Sort both lists before comparing
+    directory_files.sort()
+    file_list.sort()
+
+    # Compare the lists
+    if directory_files == file_list:
+        return True
+    else:
+        return False
 
 
 
@@ -27,7 +37,7 @@ if __name__ == '__main__':
     if sys.argv[1] == 'list':
         list_directory(directory)
     elif sys.argv[1] == 'check':
-        print(check_directory(directory))
+        check_directory(directory, )
     else:
         print('Invalid command. Need aditional argument after text.py document. EX: python text.py list or python text.py check')
         sys.exit()
