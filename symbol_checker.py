e2a67cb6b20d283810eb19c8f9ce62fecd8f5067
Nathaniel Horn <132115518+Amlis-Sanches@users.noreply.github.com>
2024-04-01 16:22:03 -0500

    need to finalize the list options.

--- symbol_checker.py
+++ symbol_checker.py
@@ -5,12 +5,15 @@ import sys
 # Specify the directory you want to scan
 directory = r'C:\Users\natha\Documents\GitHub\Crossed-Code-Letters\Photos Generated'
 
+# Specify a full path for the CSV file
+csv_file_path = 'C:\Users\natha\Documents\GitHub\Crossed-Code-Letters\Directory.csv'
+
 def list_directory(directory):
     # Get a list of all file names in the directory
     file_names = os.listdir(directory)
 
-    # Open a new CSV file in write mode
-    with open('Directory.csv', 'w', newline='') as file:
+    # Open the CSV file in write mode
+    with open(csv_file_path, 'w', newline='') as file:
         writer = csv.writer(file)
 
         # Write the list of file names to the CSV file
@@ -32,12 +35,12 @@ def check_directory(directory, file_list):
         return False
 
 
-
 if __name__ == '__main__':
     if sys.argv[1] == 'list':
         list_directory(directory)
     elif sys.argv[1] == 'check':
-        check_directory(directory, )
+        file_list = 'Directory.csv link here'
+        print(f'The files were reviewed and found to be {check_directory(directory, file_list)}.')  # Outputs: True or False
     else:
         print('Invalid command. Need aditional argument after text.py document. EX: python text.py list or python text.py check')
         sys.exit()
