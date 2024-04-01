3df64496e69d05093d3acbb6be98489f3ae8da6a
Nathaniel Horn <132115518+Amlis-Sanches@users.noreply.github.com>
2024-04-01 13:15:25 -0500

    saving new changes.

--- combine_letters.py
+++ combine_letters.py
@@ -65,30 +65,12 @@ def combine_letter(text1, text2, i, j):
 #! Split this function into two functions and prep for testing!#
 def combine_letter_in_line(blue_line, red_line):
 
-    list_of_letters = []
-    
     for i in range(len(blue_line)):
         for j in range(len(red_line)):
             bchar, bc = char_find(blue_line, i)
             rchar, rc = char_find(red_line, j)
+            bchar, bc, i, rchar, rc, j = letter_check(blue_line, red_line, bc, rc, i, j)
             
-            #* check if the characters are letters
-            while not bc or not rc: #*while both are not true
-                match (bc, rc):
-                    case (False, True):  # for Blue
-                        i += 1
-                        bchar, bc = char_find(blue_line, i)
-
-                    case (True, False):  # for Red
-                        j += 1
-                        rchar, rc = char_find(red_line, j)
-
-                    case (False, False):  # for Both
-                        i += 1
-                        j += 1
-                        bchar, bc = char_find(blue_line, i)
-                        rchar, rc = char_find(red_line, j)
-
             #TODO check if there is a space or a period+ icon and identify the end of a sentance for both red and blue
             #TODO store the information for one item into a list for the output
             
@@ -117,7 +99,28 @@ def combine_letter_in_line(blue_line, red_line):
                 '''
             
     return variables
-    
+
+def letter_check(blue_line, red_line , bc, rc, i, j):
+    #* check if the characters are letters
+    while not bc or not rc: #*while both are not true
+        match (bc, rc):
+            case (False, True):  # for Blue
+                i += 1
+                bchar, bc = char_find(blue_line, i)
+                return bchar, bc, i, rchar, rc, j
+
+            case (True, False):  # for Red
+                j += 1
+                rchar, rc = char_find(red_line, j)
+                return bchar, bc, i, rchar, rc, j
+
+            case (False, False):  # for Both
+                i += 1
+                j += 1
+                bchar, bc = char_find(blue_line, i)
+                rchar, rc = char_find(red_line, j)
+                return bchar, bc, i, rchar, rc, j
+
 def char_find(text, i):
     char, c = text[i], text[i].isalpha()
     return char, c
